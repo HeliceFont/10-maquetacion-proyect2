8cbaef98fe37a0af8e30780d4f0f3f76b3d3d154
HeliceFont <camacho485@gmail.com>
2024-05-01 20:23:58 +0200

    mobile-menu

--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -1,35 +1,79 @@
-document.addEventListener("DOMContentLoaded", (event)=>{
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
     //SELECCIONAR MIS 2 ELEMENTOS PRINCIPALES
+    const mobile_btn = document.querySelector(".navbar__mobile-btn");
+    const mobile_menu = document.querySelector(".menu-mobile");
 
-    let mobile_btn = document.querySelector(".navbar__mobile-btn");
-    let mobile_menu = document.querySelector(".menu-mobile");
-
-    // Funcion mostrar y ocultar menu
-    const showHiddenMenu = () =>{
-        let show =document.querySelector(".menu-mobile--show");
-
-        if (show){
-            mobile_menu.classList.remove("menu-mobile--show")
-        }else{
-            mobile_menu.classList.add("menu-mobile--show")
-        }
+     // Funcion mostrar y ocultar menu
+    const showHiddenMenu = () => {
+        mobile_menu.classList.toggle("menu-mobile--show");
     }
 
     // AL DAR CLICK AL BOTON DEL MENU, MOSTRAR EL MENU DE NAVEGACIÓN RESPONSIVE
-    mobile_btn.addEventListener("click", showHiddenMenu)
+    mobile_btn.addEventListener("click", showHiddenMenu);
 
     // AL REDIMENSIONAR LA PANTALLA POR DEBAJO DE 1000PX OCULTAR MENU
-    window.addEventListener("resize",()=>{
-        let window_width = parseInt(document.body.clientWidth);
-
-        if (window_width >= 1000){
-            mobile_menu.classList.remove("menu-mobile--show")
+    window.addEventListener("resize", () => {
+        if (window.innerWidth >= 1000) {
+            mobile_menu.classList.remove("menu-mobile--show");
         }
-    })
+    });
 
     // PODER CERRAR EL MENU CON EL BOTON X
-    let btn_close = document.querySelector(".menu-mobile__close")
+    let btn_close = document.querySelector(".menu-mobile__close");
 
-    btn_close.addEventListener("click",showHiddenMenu)
+    btn_close.addEventListener("click", showHiddenMenu);
+
+    //  Desplegar submenus
+    let menu_item = document.querySelectorAll(".menu-mobile__item");
     
-})
+    menu_item.forEach(item =>{
+        item.addEventListener("click", (event) =>{
+            alert(event.target.innerHTML)
+        })
+    })
+});
+
+
+
+
+// document.addEventListener("DOMContentLoaded", (event)=>{
+//     //SELECCIONAR MIS 2 ELEMENTOS PRINCIPALES
+
+//     const mobile_btn = document.querySelector(".navbar__mobile-btn");
+//     const mobile_menu = document.querySelector(".menu-mobile");
+
+//     // Funcion mostrar y ocultar menu
+//     const showHiddenMenu = () =>{
+//         let show =document.querySelector(".menu-mobile--show");
+
+//         if (show){
+//             mobile_menu.classList.remove("menu-mobile--show")
+//         }else{
+//             mobile_menu.classList.add("menu-mobile--show")
+//         }
+//     }
+
+//     // AL DAR CLICK AL BOTON DEL MENU, MOSTRAR EL MENU DE NAVEGACIÓN RESPONSIVE
+//     mobile_btn.addEventListener("click", showHiddenMenu)
+
+//     // AL REDIMENSIONAR LA PANTALLA POR DEBAJO DE 1000PX OCULTAR MENU
+//     window.addEventListener("resize",()=>{
+//         let window_width = parseInt(document.body.clientWidth);
+
+//         if (window_width >= 1000){
+//             mobile_menu.classList.remove("menu-mobile--show")
+//         }
+//     })
+
+//     // PODER CERRAR EL MENU CON EL BOTON X
+//     let btn_close = document.querySelector(".menu-mobile__close")
+
+//     btn_close.addEventListener("click",showHiddenMenu)
+
+//     // Desplegar submenus
+//     let dropdown = document.querySelectorAll()
+
+    
+// })
